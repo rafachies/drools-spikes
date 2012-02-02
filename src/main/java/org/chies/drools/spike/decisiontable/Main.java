daf8c1cec7b32fa383bf21fe991aabe486ce3204
Rafael Chies <rafachies@gmail.com>
2012-02-02 15:23:45 -0200

    first commit, basic decision table example

---
+++ src/main/java/org/chies/drools/spike/decisiontable/Main.java
@@ -0,0 +1,57 @@
+package org.chies.drools.spike.decisiontable;
+
+import java.io.StringReader;
+
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.compiler.PackageBuilder;
+import org.drools.decisiontable.InputType;
+import org.drools.decisiontable.SpreadsheetCompiler;
+import org.drools.rule.Package;
+
+public class Main {
+
+		public static void main(String[] args) {
+	        try {
+	            WorkingMemory workingMemory = createWorkingMemory();
+	            TrafficViolation trafficViolationFact = createFact();
+				insertFactAndFireRules(workingMemory, trafficViolationFact);
+	            System.out.println("Your penalty: " + trafficViolationFact.getPenalty());
+	        } catch (Throwable throwable) {
+	            throwable.printStackTrace();
+	        }
+		}
+
+		private static void insertFactAndFireRules(WorkingMemory workingMemory, TrafficViolation trafficViolationFact) {
+			workingMemory.insert(trafficViolationFact);
+			workingMemory.fireAllRules();
+		}
+
+		private static WorkingMemory createWorkingMemory() throws Exception {
+			RuleBase ruleBase = readDecisionTable();
+			WorkingMemory workingMemory = ruleBase.newStatefulSession();
+			return workingMemory;
+		}
+
+		private static TrafficViolation createFact() {
+			TrafficViolation trafficViolationFact = new TrafficViolation();
+			trafficViolationFact.setCarLicense(true);
+			trafficViolationFact.setDriverDrunk(true);
+			trafficViolationFact.setDriverLicense(true);
+			trafficViolationFact.setLights(true);
+			trafficViolationFact.setSpeed(160);
+			return trafficViolationFact;
+		}
+
+		private static RuleBase readDecisionTable() throws Exception {
+	        final SpreadsheetCompiler converter = new SpreadsheetCompiler();
+	        final String drlCompiled = converter.compile("/traffic-violation.xls", InputType.XLS);
+			PackageBuilder builder = new PackageBuilder();
+			builder.addPackageFromDrl( new StringReader( drlCompiled ) );
+			Package droolsPackage = builder.getPackage();
+			RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+			ruleBase.addPackage(droolsPackage);
+			return ruleBase;
+		}
+}
---
+++ src/main/java/org/chies/drools/spike/decisiontable/TrafficViolation.java
@@ -0,0 +1,58 @@
+package org.chies.drools.spike.decisiontable;
+
+public class TrafficViolation {
+
+	private int speed;
+	private double penalty;
+	private boolean lights;
+	private boolean carLicense;
+	private boolean driverDrunk;
+	private boolean driverLicense;
+	
+	private String test;
+	
+	
+	
+	public String getTest() {
+		return test;
+	}
+	public void setTest(String test) {
+		this.test = test;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public boolean isCarLicense() {
+		return carLicense;
+	}
+	public void setCarLicense(boolean carLicense) {
+		this.carLicense = carLicense;
+	}
+	public boolean isDriverLicense() {
+		return driverLicense;
+	}
+	public void setDriverLicense(boolean driverLicense) {
+		this.driverLicense = driverLicense;
+	}
+	public boolean isLights() {
+		return lights;
+	}
+	public void setLights(boolean lights) {
+		this.lights = lights;
+	}
+	public boolean isDriverDrunk() {
+		return driverDrunk;
+	}
+	public void setDriverDrunk(boolean driverDrunk) {
+		this.driverDrunk = driverDrunk;
+	}
+	public double getPenalty() {
+		return penalty;
+	}
+	public void setPenalty(double penalty) {
+		this.penalty = penalty;
+	}
+}
