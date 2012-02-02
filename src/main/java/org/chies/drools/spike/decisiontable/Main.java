fe625460655eecee133cf962d1777d7812f49a18
Rafael Chies <rafachies@gmail.com>
2012-02-02 17:38:01 -0200

    adding rules priority and using retract

--- src/main/java/org/chies/drools/spike/decisiontable/Main.java
+++ src/main/java/org/chies/drools/spike/decisiontable/Main.java
@@ -12,46 +12,48 @@ import org.drools.rule.Package;
 
 public class Main {
 
-		public static void main(String[] args) {
-	        try {
-	            WorkingMemory workingMemory = createWorkingMemory();
-	            TrafficViolation trafficViolationFact = createFact();
-				insertFactAndFireRules(workingMemory, trafficViolationFact);
-	            System.out.println("Your penalty: " + trafficViolationFact.getPenalty());
-	        } catch (Throwable throwable) {
-	            throwable.printStackTrace();
-	        }
+	
+	public static void main(String[] args) {
+		try {
+			WorkingMemory workingMemory = createWorkingMemory();
+			TrafficViolation trafficViolationFact = createFact();
+			insertFactAndFireRules(workingMemory, trafficViolationFact);
+			System.out.println("Your penalty: " + trafficViolationFact.getPenalty());
+		} catch (Throwable throwable) {
+			throwable.printStackTrace();
 		}
+	}
+		
 
-		private static void insertFactAndFireRules(WorkingMemory workingMemory, TrafficViolation trafficViolationFact) {
-			workingMemory.insert(trafficViolationFact);
-			workingMemory.fireAllRules();
-		}
+	private static void insertFactAndFireRules(WorkingMemory workingMemory, TrafficViolation trafficViolationFact) {
+		workingMemory.insert(trafficViolationFact);
+		workingMemory.fireAllRules();
+	}
 
-		private static WorkingMemory createWorkingMemory() throws Exception {
-			RuleBase ruleBase = readDecisionTable();
-			WorkingMemory workingMemory = ruleBase.newStatefulSession();
-			return workingMemory;
-		}
+	private static WorkingMemory createWorkingMemory() throws Exception {
+		RuleBase ruleBase = readDecisionTable();
+		WorkingMemory workingMemory = ruleBase.newStatefulSession();
+		return workingMemory;
+	}
 
-		private static TrafficViolation createFact() {
-			TrafficViolation trafficViolationFact = new TrafficViolation();
-			trafficViolationFact.setCarLicense(true);
-			trafficViolationFact.setDriverDrunk(true);
-			trafficViolationFact.setDriverLicense(true);
-			trafficViolationFact.setLights(true);
-			trafficViolationFact.setSpeed(160);
-			return trafficViolationFact;
-		}
+	private static TrafficViolation createFact() {
+		TrafficViolation trafficViolationFact = new TrafficViolation();
+		trafficViolationFact.setCarLicense(true);
+		trafficViolationFact.setDriverDrunk(true);
+		trafficViolationFact.setDriverLicense(true);
+		trafficViolationFact.setLights(true);
+		trafficViolationFact.setSpeed(170);
+		return trafficViolationFact;
+	}
 
-		private static RuleBase readDecisionTable() throws Exception {
-	        final SpreadsheetCompiler converter = new SpreadsheetCompiler();
-	        final String drlCompiled = converter.compile("/traffic-violation.xls", InputType.XLS);
-			PackageBuilder builder = new PackageBuilder();
-			builder.addPackageFromDrl( new StringReader( drlCompiled ) );
-			Package droolsPackage = builder.getPackage();
-			RuleBase ruleBase = RuleBaseFactory.newRuleBase();
-			ruleBase.addPackage(droolsPackage);
-			return ruleBase;
-		}
+	private static RuleBase readDecisionTable() throws Exception {
+		final SpreadsheetCompiler converter = new SpreadsheetCompiler();
+		final String drlCompiled = converter.compile("/traffic-violation.xls", InputType.XLS);
+		PackageBuilder builder = new PackageBuilder();
+		builder.addPackageFromDrl( new StringReader( drlCompiled ) );
+		Package droolsPackage = builder.getPackage();
+		RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+		ruleBase.addPackage(droolsPackage);
+		return ruleBase;
+	}
 }
--- src/main/java/org/chies/drools/spike/decisiontable/TrafficViolation.java
+++ src/main/java/org/chies/drools/spike/decisiontable/TrafficViolation.java
@@ -9,16 +9,7 @@ public class TrafficViolation {
 	private boolean driverDrunk;
 	private boolean driverLicense;
 	
-	private String test;
 	
-	
-	
-	public String getTest() {
-		return test;
-	}
-	public void setTest(String test) {
-		this.test = test;
-	}
 	public int getSpeed() {
 		return speed;
 	}
